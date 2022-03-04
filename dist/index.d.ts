5dda7a3356d246116b0d182c6b5f77c5608a060e
jeromevvb <jeromevvb@gmail.com>
2022-03-04 15:45:48 +0700

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,4 +5,18 @@ interface ButtonProps {
 }
 declare const Button: React.FC<ButtonProps>;
 
-export { Button };
+interface TypographyProps {
+    size?: 'small' | 'medium' | 'large';
+    weight?: 'normal' | 'medium' | 'semibold';
+    className?: string;
+    caption?: boolean;
+    color?: 'muted' | 'dark' | 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success';
+    uppercase?: boolean;
+    tag?: 'p' | 'span';
+    ellipsis?: boolean;
+    hoverAsLink?: boolean;
+    lineBreaks?: boolean;
+}
+declare const Typography: React.FC<TypographyProps>;
+
+export { Button, Typography };
