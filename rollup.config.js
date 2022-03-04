061df9a5b3af2ecde2427e87a2066b54bc649378
jeromevvb <jeromevvb@gmail.com>
2022-03-04 17:08:30 +0700

    test

--- rollup.config.js
+++ rollup.config.js
@@ -25,18 +25,18 @@ export default [
     plugins: [
       resolve(),
       commonjs({ exclude: 'node_modules' }),
-      typescript({ tsconfig: './tsconfig.json' }),
-      babel({
-        babelHelpers: 'bundled',
-        extensions: [...DEFAULT_EXTENSIONS, '.tsx', '.ts'],
-        presets: [
-          [
-            '@babel/preset-react',
-            { runtime: 'automatic', importSource: '@emotion/react' }
-          ]
-        ],
-        plugins: ['@emotion/babel-plugin']
-      })
+      typescript({ tsconfig: './tsconfig.json' })
+      // babel({
+      //   babelHelpers: 'bundled',
+      //   extensions: [...DEFAULT_EXTENSIONS, '.tsx', '.ts'],
+      //   presets: [
+      //     [
+      //       '@babel/preset-react',
+      //       { runtime: 'automatic', importSource: '@emotion/react' }
+      //     ]
+      //   ],
+      //   plugins: ['@emotion/babel-plugin']
+      // })
     ]
   },
   {
