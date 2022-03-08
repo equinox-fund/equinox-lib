d17bdfe13a366fb2cbad604bb93bef2c9726ddc8
jeromevvb <jeromevvb@gmail.com>
2022-03-08 14:06:20 +0700

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,14 @@
 import React$1, { ReactNode } from 'react';
 import { IconType } from 'react-icons';
+import { Placement } from 'react-overlays/usePopper';
 
 declare type TIconColor = 'primary' | 'secondary' | 'warning' | 'muted' | 'success' | 'error' | 'info';
+interface IconProps {
+    svg: IconType;
+    className?: string;
+    color?: TIconColor;
+}
+declare const Icon: React$1.FC<IconProps>;
 
 interface ButtonProps {
     children?: ReactNode;
@@ -40,4 +47,20 @@ interface TypographyProps {
 }
 declare const Typography: React$1.FC<TypographyProps>;
 
-export { Button, Typography };
+interface TooltipProps {
+    children?: ReactNode;
+    placement?: Placement;
+    className?: string;
+    label?: string;
+}
+declare const Tooltip: React$1.FC<TooltipProps>;
+
+interface SpinnerProps {
+    size?: 'small' | 'medium' | 'large';
+    className?: string;
+}
+declare const Spinner: React$1.FC<SpinnerProps>;
+
+declare const useWallet: () => void;
+
+export { Button, Icon, Spinner, Tooltip, Typography, useWallet };
