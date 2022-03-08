70cb5ee6a262d2153f37b77249d4aa635a5a3ebe
jeromevvb <jeromevvb@gmail.com>
2022-03-08 13:46:48 +0700

    build dist

--- dist/esm/types/types/theme.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type Color = 'muted' | 'dark' | 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success';
-export declare type Size = 'small' | 'medium' | 'large';
-export declare type FontWeight = 'normal' | 'medium' | 'semibold';
--- src/types/twin.d.ts
+++ src/types/twin.d.ts
@@ -7,3 +7,4 @@ declare module 'twin.macro' {
   const styled: typeof styledImport
   const css: typeof cssImport
 }
+ 
