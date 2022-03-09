a1f0a8decc55cad8b211565265f64c37d30bb841
jeromevvb <jeromevvb@gmail.com>
2022-03-09 16:51:27 +0700

    Update DisplayDate.stories.tsx

--- src/components/common/DisplayDate/DisplayDate.stories.tsx
+++ src/components/common/DisplayDate/DisplayDate.stories.tsx
@@ -12,8 +12,26 @@ const Template: Story<DisplayDateProps> = (args) => (
   </div>
 )
 
-export const Default = Template.bind({})
-Default.args = {
+export const FormatDate = Template.bind({})
+FormatDate.args = {
   date: '2022-05-19T01:00:00.000+00:00',
   format: 'date'
+}
+
+export const FormatTimeTo = Template.bind({})
+FormatTimeTo.args = {
+  date: '2022-05-19T01:00:00.000+00:00',
+  format: 'timeTo'
+}
+
+export const FormatDateTime = Template.bind({})
+FormatDateTime.args = {
+  date: '2022-05-19T01:00:00.000+00:00',
+  format: 'dateTime'
+}
+
+export const FormatTime = Template.bind({})
+FormatTime.args = {
+  date: '2022-05-19T01:00:00.000+00:00',
+  format: 'time'
 }
