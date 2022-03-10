2f0425dd09be6e63f5242783f5df23c9ea094806
Simon Neveu <simonneveu@gmail.com>
2022-03-10 12:58:28 +0700

    added fallback for images already loaded to image component

--- src/components/common/Image/Image.tsx
+++ src/components/common/Image/Image.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import styles from './styles'
 
 export interface ImageProps {
@@ -17,16 +17,25 @@ const Image: React.FC<ImageProps> = ({
   showPreloader = true
 }) => {
   const [loading, setLoading] = useState(true)
+  const imgRef = useRef<HTMLImageElement | null>(null)
 
   const handleImageLoaded = () => {
     setTimeout(() => setLoading(false), 50)
     if (onLoad) onLoad()
   }
 
+  useEffect(function () {
+    if (imgRef.current?.complete) {
+      setTimeout(function () {
+        return setLoading(false)
+      }, 50)
+    }
+  }, [])
+
   return (
     <div className={className}>
       <div css={styles({ loading, showPreloader })}>
-        <img src={src} alt={alt} onLoad={handleImageLoaded} />
+        <img src={src} alt={alt} onLoad={handleImageLoaded} ref={imgRef} />
       </div>
     </div>
   )
