b6a0ad6c1703aa9962005daf4d29ee0a01648eb5
Simon Neveu <simonneveu@gmail.com>
2022-04-05 12:52:50 +0700

    added overflow gradients to rounded tabs

--- src/components/common/RoundedTabs/RoundedTabs.tsx
+++ src/components/common/RoundedTabs/RoundedTabs.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Typography from '../Typography'
 import styles from './styles'
 
@@ -17,6 +17,48 @@ const RoundedTabs: React.FC<RoundedTabsProps> = ({
   onClickTab,
   activeTab
 }) => {
+  const tabsEl = useRef(null)
+  const _scrolled = useRef(false)
+  const [scrolled, setScrolled] = useState(false)
+  const _showOverflowGradient = useRef(false)
+  const [showOverflowGradient, setShowOverflowGradient] = useState(false)
+
+  useEffect(() => {
+    handleResize()
+  }, [])
+
+  const handleResize = () => {
+    if (tabsEl.current.scrollWidth > tabsEl.current.clientWidth) {
+      if (!_showOverflowGradient.current) {
+        setShowOverflowGradient(true)
+        _showOverflowGradient.current = true
+      }
+    } else if (_showOverflowGradient.current) {
+      setShowOverflowGradient(false)
+      _showOverflowGradient.current = false
+    }
+  }
+
+  const handleScroll = () => {
+    if (tabsEl.current.scrollLeft > 0 && !_scrolled.current) {
+      setScrolled(true)
+      _scrolled.current = true
+    } else if (tabsEl.current.scrollLeft === 0 && _scrolled.current) {
+      setScrolled(false)
+      _scrolled.current = false
+    }
+  }
+
+  useEffect(() => {
+    tabsEl.current.addEventListener('scroll', handleScroll)
+    window?.addEventListener('resize', handleResize)
+
+    return () => {
+      tabsEl.current.removeEventListener('scroll', handleScroll)
+      window?.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   const elements = tabs.map(({ label, value }) => {
     return (
       <button
@@ -33,8 +75,10 @@ const RoundedTabs: React.FC<RoundedTabsProps> = ({
   })
 
   return (
-    <div css={styles}>
-      <div className="tabs">{elements}</div>
+    <div css={styles({ scrolled, showOverflowGradient })}>
+      <div className="tabs-container" ref={tabsEl}>
+        <div className="tabs">{elements}</div>
+      </div>
     </div>
   )
 }
